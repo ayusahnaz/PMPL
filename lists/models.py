a755e4b3aece227211d203dc382ac0a0dcb82e07
Ayu <ayusahnaz@gmail.com>
2015-09-22 13:49:25 +0700

    Adjusting the Rest of the World to Our New Models

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+# Create your models here.
+class List(models.Model):
+	pass
+
 class Item(models.Model):
 	text = models.TextField(default='')
-# Create your models here.
+	list = models.ForeignKey(List, default=None)
+
+
